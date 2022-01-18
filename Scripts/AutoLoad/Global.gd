9ac15a73bbcacebee32fe06121e9f3f724134c47
Programmer Cheema <47918386+bilalCh213@users.noreply.github.com>
2022-01-18 17:02:48 +0500

    Added Pause Toggle with P/Escape Key

--- Scripts/AutoLoad/Global.gd
+++ Scripts/AutoLoad/Global.gd
@@ -5,12 +5,12 @@ signal shake(val)
 var ortho_camera: Camera = null
 var raycast_position = null
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pause_mode = Node.PAUSE_MODE_PROCESS
 
 func set_camera(cam: Camera):
 	ortho_camera = cam
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().paused = !get_tree().paused
