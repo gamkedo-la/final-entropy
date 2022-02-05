7c59fd7aeabc3bdde7d85fec4df602240d711653
Programmer Cheema <47918386+bilalCh213@users.noreply.github.com>
2022-02-05 20:06:22 +0500

    Added Multiple Bullet Patterns Mechanic via Shot Resource.

--- Scripts/Enemies/Experimental/Gun.gd
+++ Scripts/Enemies/Experimental/Gun.gd
@@ -3,42 +3,32 @@ extends Position3D
 export (PackedScene) var bullet_path
 var BULLET
 onready var rnd = RandomNumberGenerator.new() #rnd.randf_range(min,max)
-export var shotDelay = 0.5
-export var reloadDelay = 2.0
-export var reloadAfterShots = 4
-export var bulletSpeed = 0.5
-export var bulletsPerShot = 12
-export var angleOffsetPerShot = 10
+export (Array, Resource) var shots
 
-var shotTimer = 0
-var reloadTimer = 0
-var shots = 0
+var index = 0
+var timer = 0
 var angleOffset = 0
 
 func _ready():
 	rnd.randomize()
 	BULLET = bullet_path
-	reloadTimer = reloadDelay
+	timer = shots[index].delay
 
 func _process(delta):
-	if reloadTimer <= 0:
-		if shotTimer <= 0:
-			for i in range(bulletsPerShot):
-				var bullet = BULLET.instance()
-				add_child(bullet)
-				bullet.set_as_toplevel(true)
-				bullet.transform = global_transform
-				bullet.collision_layer = 0b00000000000000000100
-				bullet.collision_mask = 0b00000000000000000011
-				bullet.velocity.x = bulletSpeed * cos(deg2rad(angleOffset + ((i / float(bulletsPerShot)) * 360.0)))
-				bullet.velocity.z = bulletSpeed * sin(deg2rad(angleOffset + ((i / float(bulletsPerShot)) * 360.0)))
-			angleOffset += angleOffsetPerShot
-			shotTimer = shotDelay
-			shots += 1
-			if shots >= reloadAfterShots:
-				shots = 0
-				reloadTimer = reloadDelay
-		else:
-			shotTimer -= delta
+	if timer <= 0:
+		for i in range(shots[index].bullets):
+			var bullet = BULLET.instance()
+			add_child(bullet)
+			bullet.set_as_toplevel(true)
+			bullet.transform = global_transform
+			bullet.collision_layer = 0b00000000000000000100
+			bullet.collision_mask = 0b00000000000000000011
+			bullet.velocity.x = shots[index].bulletSpeed * cos(deg2rad(angleOffset + ((i / float(shots[index].bullets)) * 360.0)))
+			bullet.velocity.z = shots[index].bulletSpeed * sin(deg2rad(angleOffset + ((i / float(shots[index].bullets)) * 360.0)))
+		angleOffset += shots[index].angleOffset
+		timer = shots[index].delay
+		index = index + 1
+		if index >= shots.size():
+			index = 0
 	else:
-		reloadTimer -= delta
+		timer -= delta
---
+++ Scripts/Enemies/Experimental/ShotData.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name Shot
+
+export var delay = 0.5
+export var bullets = 12
+export var bulletSpeed = 0.5
+export var angleOffset = 10
