7d06eece63a6c9d0c3b8face666d553fef9a7440
Michael Fewkes <mdfewkes@gmail.com>
2022-04-01 15:48:44 -0600

    Generation test scene

--- Scripts/RoomGen/LevelGeneration.gd
+++ Scripts/RoomGen/LevelGeneration.gd
@@ -6,11 +6,12 @@ var level_length = 0
 var main_path = []
 
 func _ready():
-	level_length = randi() % (room_container.max_rooms - room_container.min_rooms) + room_container.min_rooms
+	level_length = (randi() % (room_container.max_rooms - room_container.min_rooms)) + room_container.min_rooms
 	
 	# Generate main path
 	for i in level_length:
 		var new_room = room_container.common_rooms[randi() % room_container.common_rooms.size()].instance()
+		add_child(new_room)
 		main_path.push_back((new_room))
 		new_room.room_name = "M" + str(i)
 		new_room.translation = Vector3(0, 0, room_container.vertical_seperation * 1)
@@ -19,11 +20,14 @@ func _ready():
 		if i != 0:
 			new_room.return_room = "M" + str(i-1)
 			
+		for j in new_room.room_portals:
+			new_room.connected_rooms.push_back("")
+		
 		# Link to next room
 		if i+1 != level_length:
 			var portal_index = randi() % new_room.room_portals.size()
-			new_room.connected_rooms[portal_index] = "M" + str(i+1)
-			
-		# Add special branches
+			new_room.connected_rooms[portal_index] = "M" + str((i+1))
 		
-		# Add Boss
+	# Add Boss
+			
+	# Add special branches
