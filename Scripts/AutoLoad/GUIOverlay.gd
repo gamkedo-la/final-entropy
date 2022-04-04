97bf2875b7c81a91d66e661b1533544987983725
Randy Tan Shaoxian <oneseedfruit@gmail.com>
2022-04-05 04:28:02 +0800

    "Paused" text animation on unpause

--- Scripts/AutoLoad/GUIOverlay.gd
+++ Scripts/AutoLoad/GUIOverlay.gd
@@ -5,13 +5,16 @@ onready var Muted = $GUIOverlayIndicators/Muted
 
 
 func _ready():
-	Menu.hide()
 	toggle_muted()
 
 
 func toggle_menu(is_visible = true):
-	Menu.visible = is_visible
+	Menu.toggle(is_visible)
 
 
 func toggle_muted(is_muted = false):
 	Muted.visible = is_muted
+
+
+func _on_GUIOverlayMenu_menu_toggled(is_visible):
+	Menu.visible = is_visible
--- Scripts/AutoLoad/GUIOverlayMenu.gd
+++ Scripts/AutoLoad/GUIOverlayMenu.gd
@@ -1,8 +1,25 @@
 extends Panel
 
+signal menu_toggled(is_visible)
+
+var _is_visible = false
+
 onready var Animation = $AnimationPlayer
 
 
-func _on_GUIOverlayMenu_visibility_changed():
-	if visible:
+func _ready():
+	hide()
+
+
+func toggle(is_visible):
+	_is_visible = is_visible
+	if is_visible and not Animation.is_playing():
+		show()
 		Animation.play("Paused")
+	else:
+		Animation.play_backwards("Paused")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name:String):
+	if anim_name == "Paused": # Replace with function body.
+		emit_signal("menu_toggled", _is_visible)
