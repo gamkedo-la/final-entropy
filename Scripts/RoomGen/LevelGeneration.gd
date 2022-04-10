621b995f3ff32de441b77dffa2374e00234de15a
Michael Fewkes <mdfewkes@gmail.com>
2022-04-10 11:04:51 -0600

    Generator now places boss rooms

--- Scripts/RoomGen/LevelGeneration.gd
+++ Scripts/RoomGen/LevelGeneration.gd
@@ -12,14 +12,15 @@ func _ready():
 	for i in level_length:
 		var new_room = room_container.common_rooms[randi() % room_container.common_rooms.size()].instance()
 		add_child(new_room)
-		main_path.push_back((new_room))
 		new_room.room_name = "M" + str(i)
-		new_room.translation = Vector3(0, 0, room_container.vertical_seperation * 1)
+		new_room.name = new_room.room_name
+		new_room.translation = Vector3(0, 0, room_container.vertical_seperation * i)
 		
-		# Set Return rooms
+		# Set Return room
 		if i != 0:
 			new_room.return_room = "M" + str(i-1)
 			
+		# Prep room links
 		for j in new_room.room_portals:
 			new_room.connected_rooms.push_back("")
 		
@@ -28,6 +29,26 @@ func _ready():
 			var portal_index = randi() % new_room.room_portals.size()
 			new_room.connected_rooms[portal_index] = "M" + str((i+1))
 		
+		main_path.push_back((new_room))
+		
 	# Add Boss
+	var boss_room = room_container.common_rooms[randi() % room_container.common_rooms.size()].instance()
+	add_child(boss_room)
+	boss_room.room_name = "B0"
+	boss_room.name = boss_room.room_name
+	boss_room.translation = Vector3(0, 0, room_container.vertical_seperation * main_path.size())
+	
+	# Set Return room
+	boss_room.return_room = "M" + str(main_path.size()-1)
+	
+	# Connect last room
+	var last_room = main_path[main_path.size()-1]
+	var portal_index = randi() % last_room.room_portals.size()
+	last_room.connected_rooms[portal_index] = "B0"
+	
+	main_path.push_back((boss_room))
+	
+	
+	
 			
 	# Add special branches
