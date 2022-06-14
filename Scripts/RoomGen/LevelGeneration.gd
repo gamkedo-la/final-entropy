a49bed792aeb5cd846a9b3d7977e1f72199c2f25
Michael Fewkes <mdfewkes@gmail.com>
2022-06-13 22:17:22 -0600

    Room generator now creates branches

--- Scripts/RoomGen/LevelGeneration.gd
+++ Scripts/RoomGen/LevelGeneration.gd
@@ -9,46 +9,67 @@ func _ready():
 	level_length = (randi() % (room_container.max_rooms - room_container.min_rooms)) + room_container.min_rooms
 	
 	# Generate main path
+	var step = 0
 	for i in level_length:
+		# Create new room
 		var new_room = room_container.common_rooms[randi() % room_container.common_rooms.size()].instance()
 		add_child(new_room)
 		new_room.room_name = "M" + str(i)
 		new_room.name = new_room.room_name
-		new_room.translation = Vector3(0, 0, room_container.vertical_seperation * i)
-		
-		# Set Return room
-		if i != 0:
-			new_room.return_room = "M" + str(i-1)
+		new_room.translation = Vector3(0, 0, 50 * i)
 			
 		# Prep room links
 		for j in new_room.room_portals:
 			new_room.connected_rooms.push_back("")
 		
-		# Link to next room
-		if i+1 != level_length:
-			var portal_index = randi() % new_room.room_portals.size()
-			new_room.connected_rooms[portal_index] = "M" + str((i+1))
+		# If not the first room
+		if i != 0:
+			# Link last room to this room
+			var room_found = false
+			while !room_found:
+				# No rooms lower than 0, cycle to the top
+				if step <= 0:
+					step = main_path.size()-1
+				
+				# Check for open portal nodes
+				var open_nodes = []
+				for j in main_path[step-1].connected_rooms.size():
+					if main_path[step-1].connected_rooms[j] == "":
+						open_nodes.push_back(j)
+				# Assign new room to last rooms portal
+				if open_nodes.size() != 0:
+					room_found = true
+					var portal_index = open_nodes[randi() % open_nodes.size()]
+					main_path[step-1].connected_rooms[portal_index] = "M" + str(i)
+				# If no open portals, step back a room
+				else:
+					step -= 1
+				
+			# Set Return room
+			new_room.return_room = main_path[step-1].room_name
 		
-		main_path.push_back((new_room))
+		main_path.push_back(new_room)
+		
+		step += 1
+		# Branch back
+		if randf() < room_container.odds_of_branching:
+			step -= 1
+		
 		
 	# Add Boss
 	var boss_room = room_container.boss_rooms[randi() % room_container.boss_rooms.size()].instance()
 	add_child(boss_room)
 	boss_room.room_name = "B0"
 	boss_room.name = boss_room.room_name
-	boss_room.translation = Vector3(0, 0, room_container.vertical_seperation * main_path.size())
+	boss_room.translation = Vector3(0, 0, 50)
 	
-	# Set Return room
+	# Set Boss Return room
 	boss_room.return_room = "M" + str(main_path.size()-1)
 	
-	# Connect last room
+	# Connect last room to boss room
 	var last_room = main_path[main_path.size()-1]
 	var portal_index = randi() % last_room.room_portals.size()
 	last_room.connected_rooms[portal_index] = "B0"
 	
 	main_path.push_back((boss_room))
 	
-	
-	
-			
-	# Add special branches
--- Scripts/RoomGen/RoomContainer.gd
+++ Scripts/RoomGen/RoomContainer.gd
@@ -4,9 +4,7 @@ class_name RoomContainer
 
 export (int) var min_rooms = 5
 export (int) var max_rooms = 8
-export (float) var odds_of_special_room
-export (int) var vertical_seperation = 50
-export (int) var horizontal_seperation = 50
+export (float) var odds_of_branching #0-1
 export (Array, PackedScene) var boss_rooms setget set_boss_rooms
 export (Array, PackedScene) var special_rooms setget set_special_rooms
 export (Array, PackedScene) var common_rooms setget set_common_rooms
